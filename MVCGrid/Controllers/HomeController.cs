d812f9c84674f2fff44f3f91cd96fb64ccac6f49
Tushar Gupta <Tushar.Gupta@egonzehnder.com>
2018-11-01 18:08:21 +0530

    Add project files.

---
+++ MVCGrid/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MVCGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        string[] cities = { "NYC", "Delhi", "London", "Gurgaon", "Noida", "Faridabad" };
+        Random rnd = new Random();
+        List<Employee> empCollection;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(CreateData());
+        }
+
+        private string GetRandomCity()
+        {
+            try
+            {
+                return cities[rnd.Next(1, 6)];
+            }
+            catch (Exception)
+            {
+                return cities[0];
+            }
+        }
+
+        private List<Employee> CreateData()
+        {
+            empCollection = new List<Employee>();
+
+            Random rnd1 = new Random(50);
+
+            for (int i = 0; i < 100; i++)
+            {
+                empCollection.Add(new Employee()
+                {
+                    Id = i,
+                    Name = "Emp " + rnd.Next(5000),
+                    City = GetRandomCity(),
+                    EmpID = "EMP" + i
+                });
+            }
+
+            return empCollection;
+        }
+    }
+}
---
+++ MVCGrid/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGrid.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string EmpID { get; set; }
+
+        public string City { get; set; }
+    }
+}
